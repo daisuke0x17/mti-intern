b20eeb981e952d4a3f06de7eeed4bcc6b9184f6e
aoi0412 <aoiaoi0412.1203@gmail.com>
2022-08-26 18:09:06 +0000

    firebaseを導入

--- src/main.js
+++ src/main.js
@@ -2,10 +2,22 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import css from "./assets/style.css";
-
+import {initializeApp} from 'firebase/app';
 Vue.config.productionTip = false
+const config = {
+  apiKey: "AIzaSyC6ZLG7b0ovlOmdSn1YQ0GdrV0w0lMo1Rs",
+  authDomain: "disaster5.firebaseapp.com",
+  projectId: "disaster5",
+  storageBucket: "disaster5.appspot.com",
+  messagingSenderId: "731796188239",
+  appId: "1:731796188239:web:b849043d1b23c6d3a20e3a",
+  measurementId: "G-W1C30PP91B"
+};
+
+initializeApp(config);
+
 new Vue({
   router,
   css,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
